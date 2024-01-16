6e8e7c08d5f38bf5a9d0f8014cb56d699387d75f
laurabp99 <laurabennasar99@gmail.com>
2024-01-16 15:43:59 +0100

    commit martes tarde

--- src/table.js
+++ src/table.js
@@ -209,32 +209,7 @@ class Table extends HTMLElement {
                             </ul>
                         </div>
                     </div>
-                    <div class="register">
-                        <div class="modify-register">
-                            <div class="edit-button">
-                                <svg xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24">
-                                    <title>pencil</title>
-                                    <path
-                                        d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
-                                </svg>
-                            </div>
-                            <div class="delete-button">
-                                <svg xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24">
-                                    <title>delete</title>
-                                    <path
-                                        d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
-                                </svg>
-                            </div>
-                        </div>
-                        <div class="register-data">
-                            <ul>
-                                <li>Email</li>
-                                <li>Nombre</li>
-                                <li>Apellidos</li>
-                            </ul>
-                        </div>
+
                     </div>
                 </div>
             </div>
@@ -244,12 +219,12 @@ class Table extends HTMLElement {
 
         tableSection?.addEventListener('click', async (event) => {
 
-            if(event.target.closest('.filter-button')){
+            if (event.target.closest('.filter-button')) {
                 document.dispatchEvent(new CustomEvent('showFilterModal'))
             }
 
             if (event.target.closest('.edit-button')) {
-                
+
             }
 
             if (event.target.closest('.delete-button')) {
