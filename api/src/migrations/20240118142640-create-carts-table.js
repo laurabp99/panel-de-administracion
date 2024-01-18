ba73590852efd9ebcaa3ee0c6a5561a8e13e5057
laurabp99 <laurabennasar99@gmail.com>
2024-01-18 15:43:27 +0100

    commit jueves tarde

---
+++ api/src/migrations/20240118142640-create-carts-table.js
@@ -0,0 +1,40 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('carts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      uuid: {
+        allowNull: false,
+        type: Sequelize.UUID
+      },
+      customerId: {
+        type: Sequelize.INTEGER
+      },
+      fingerprintId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('carts')
+  }
+}
