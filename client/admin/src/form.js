bcba3f87e3eb7744d62ad92388df4abcc7434f5a
laurabp99 <laurabennasar99@gmail.com>
2024-01-31 15:34:47 +0100

    language tabs

--- client/admin/src/form.js
+++ client/admin/src/form.js
@@ -125,106 +125,106 @@ class Form extends HTMLElement {
             }
             </style>
 
-          <div class='form'>
-            <div class='form-top'>
-              <div class='tabs'>
-                <div class='tab active' data-tab='general'>
-                  <span>Principal</span>
+          <div class='form-container'>
+            <div class='form'>
+              <div class='form-top'>
+                <div class='tabs'>
+                  <div class='tab active' data-tab='general'>
+                    <span>Principal</span>
+                  </div>
+                  <div class='tab' data-tab='images'>
+                    <span>Imágenes</span>
+                  </div>
                 </div>
-                <div class='tab' data-tab='images'>
-                  <span>Imágenes</span>
-                </div>
-              </div>
-              <div class='form-buttons'>
-                <div class='form-clear'>
-                  <svg xmlns='http://www.w3.org/2000/svg'
-                        viewBox='0 0 24 24'>
-                      <title>broom</title>
-                      <path
-                                d='M19.36,2.72L20.78,4.14L15.06,9.85C16.13,11.39 16.28,13.24 15.38,14.44L9.06,8.12C10.26,7.22 12.11,7.37 13.65,8.44L19.36,2.72M5.93,17.57C3.92,15.56 2.69,13.16 2.35,10.92L7.23,8.83L14.67,16.27L12.58,21.15C10.34,20.81 7.94,19.58 5.93,17.57Z' />
-                      </svg>
-                </div>
-                <div class='form-save'>
-                  <svg xmlns='http://www.w3.org/2000/svg'
-                    viewBox='0 0 24 24'>
-                      <title>content-save</title>
+                <div class='form-buttons'>
+                  <div class='form-clear'>
+                    <svg xmlns='http://www.w3.org/2000/svg'
+                          viewBox='0 0 24 24'>
+                        <title>broom</title>
                         <path
-                        d='M15,9H5V5H15M12,19A3,3 0 0,1 9,16A3,3 0 0,1 12,13A3,3 0 0,1 15,16A3,3 0 0,1 12,19M17,3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V7L17,3Z' />
-                  </svg>
+                                  d='M19.36,2.72L20.78,4.14L15.06,9.85C16.13,11.39 16.28,13.24 15.38,14.44L9.06,8.12C10.26,7.22 12.11,7.37 13.65,8.44L19.36,2.72M5.93,17.57C3.92,15.56 2.69,13.16 2.35,10.92L7.23,8.83L14.67,16.27L12.58,21.15C10.34,20.81 7.94,19.58 5.93,17.57Z' />
+                        </svg>
+                  </div>
+                  <div class='form-save'>
+                    <svg xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 24 24'>
+                        <title>content-save</title>
+                          <path
+                          d='M15,9H5V5H15M12,19A3,3 0 0,1 9,16A3,3 0 0,1 12,13A3,3 0 0,1 15,16A3,3 0 0,1 12,19M17,3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V7L17,3Z' />
+                    </svg>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <form action>
-            <div class='tab-content active' data-tab='general'>
-              <div class='form-row'>
-                <div class='form-element'>
-                  <div class='form-label'>
-                    <label>Nombre</label>
-                  </div>
-                  <div class='form-input'>
-                    <input type='text' class='name-validation' data-onlyletters='true'>
-                  </div>
-                </div>
-                <div class='form-element'>
-                  <div class='form-label'>
-                    <label>Email</label>
-                  </div>
-                  <div class='form-input'>
-                    <input type='email'>
-                  </div>
-                </div>
-              </div>
-              <div class='form-row'>
-                <div class='form-element'>
-                  <div class='form-label'>
-                    <label>Password</label>
-                  </div>
-                  <div class='form-input'>
-                    <input type='password' class='validate' data-minlength='8'>
-                  </div>
-                </div>
-                <div class='form-element'>
-                  <div class='form-label'>
-                    <label>Repetir password</label>
-                  </div>
-                  <div class='form-input'>
-                    <input type='password'>
-                  </div>
-                </div>
-              </div>
-                <div class='languages-tabs'>
-
-                  <div class='tabs'>
-                    <div class='tab active' data-tab='español'>
-                      <h2>es</h2>
-                    </div>
-                    <div class='tab' data-tab='inglés'>
-                      <h2>en</h2>
-                    </div>
-                  </div>
-
-                </div>
-                <div class='tab-content active' data-tab='español'><h3>holi</h3></div>
-                <div class='tab-content' data-tab='inglés'><h3>ola</h3></div>
-              </div>
-              <div class='tab-content' data-tab='images'>
+            <form action>
+              <div class='tab-content active' data-tab='general'>
                 <div class='form-row'>
                   <div class='form-element'>
                     <div class='form-label'>
-                      <label>Avatar</label>
+                      <label>Nombre</label>
                     </div>
                     <div class='form-input'>
-                      <input type='file'>
+                      <input type='text' class='name-validation' data-onlyletters='true'>
+                    </div>
+                  </div>
+                  <div class='form-element'>
+                    <div class='form-label'>
+                      <label>Email</label>
+                    </div>
+                    <div class='form-input'>
+                      <input type='email'>
+                    </div>
+                  </div>
+                </div>
+                <div class='form-row'>
+                  <div class='form-element'>
+                    <div class='form-label'>
+                      <label>Password</label>
+                    </div>
+                    <div class='form-input'>
+                      <input type='password' class='validate' data-minlength='8'>
+                    </div>
+                  </div>
+                  <div class='form-element'>
+                    <div class='form-label'>
+                      <label>Repetir password</label>
+                    </div>
+                    <div class='form-input'>
+                      <input type='password'>
+                    </div>
+                  </div>
+                </div>
+                  <div class='languages-tabs'>
+                    <div class='tabs'>
+                      <div class='tab active' data-tab='español'>
+                        <h2>es</h2>
+                      </div>
+                      <div class='tab' data-tab='inglés'>
+                        <h2>en</h2>
+                      </div>
+                    </div>
+                  </div>
+                  <div class='tab-content active' data-tab='español'><h3>holi</h3></div>
+                  <div class='tab-content' data-tab='inglés'><h3>ola</h3></div>
+                </div>
+                <div class='tab-content' data-tab='images'>
+                  <div class='form-row'>
+                    <div class='form-element'>
+                      <div class='form-label'>
+                        <label>Avatar</label>
+                      </div>
+                      <div class='form-input'>
+                        <input type='file'>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </form>  
+            </form>
+          </div> 
           `
 
-    const form = this.shadow.querySelector('.form')
+    const form = this.shadow.querySelector('.form-container')
 
     form.addEventListener('input', (event) => {
       if (event.target.closest('.validate')) {
@@ -254,7 +254,8 @@ class Form extends HTMLElement {
     form.addEventListener('click', (event) => {
       const tab = event.target.closest('.tab')
       const lastActiveTab = tab.parentNode.querySelector('.active')
-      if (tab) {
+      console.log('uwu')
+      if (tab) { 
         lastActiveTab.classList.remove('active')
         tab.classList.add('active')
         console.log(tab)
